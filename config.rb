4650588f475a1be324744f296e593ce6a0104710
lexxbrose <lexxbrose@gmail.com>
2014-10-17 11:05:21 +0300

    hb_1017

---
+++ config.rb
@@ -0,0 +1,37 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "css"
+sass_dir = "sass"
+fonts_dir = "fonts"
+images_dir = "img"
+javascripts_dir = "js"
+
+relative_assets = true
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+preferred_syntax = :scss
+module Sass::Script::Functions
+  def asset_url(string)
+    assert_type string, :String
+    Sass::Script::String.new("url('../img/#{string.value}')")
+  end
+  alias_method :"asset-url",:asset_url 
+  declare :"asset-url", :args => [:string]
+end
---
+++ js/script.js
@@ -0,0 +1,6 @@
+$(function(){
+	$('.navbar-toggle').click(function(){$('body').toggleClass('navbar-wrap');return false;});
+	if($('.case-sort').length){$(window).on('load resize',function() {var windowWidth = $(window).width(); if (windowWidth < 960) {$('.case-sort .current a').click(function(){$('.case-sort').toggleClass('case-sort_open');return false;});} }); };
+	if($('#carousel').length){$('#carousel').carouFredSel({auto:false,responsive:true,scroll:1,prev:'#carousel-prev',next:'#carousel-next',pagination:'#carousel-pag',items:{visible:1,width:740,height:'60%'},swipe:{onMouse:true,onTouch:true}});};
+	$('.case-faq .item .capt a').click(function(){$(this).parents('.item').toggleClass('item_open');return false;});
+});
