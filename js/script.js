e0c6ba55fd2f3bb1a8490106e6495183b62b90a2
lexxbrose <lexxbrose@gmail.com>
2014-10-22 18:28:44 +0300

    hb_1022

--- js/script.js
+++ js/script.js
@@ -3,4 +3,6 @@ $(function(){
 	if($('.case-sort').length){$(window).on('load resize',function() {var windowWidth = $(window).width(); if (windowWidth < 960) {$('.case-sort .current a').click(function(){$('.case-sort').toggleClass('case-sort_open');return false;});} }); };
 	if($('#carousel').length){$('#carousel').carouFredSel({auto:false,responsive:true,scroll:1,prev:'#carousel-prev',next:'#carousel-next',pagination:'#carousel-pag',items:{visible:1,width:740,height:'60%'},swipe:{onMouse:true,onTouch:true}});};
 	$('.case-faq .item .capt a').click(function(){$(this).parents('.item').toggleClass('item_open');return false;});
+	$('.switcher label').click(function(){$(this).addClass('current').siblings().removeClass('current');});
+	if($('.chosen-select').length){$('.chosen-select').chosen({});};
 });
