3fd1d531feef5715398a5ce2a44d4137c3620b02
lexxbrose <lexxbrose@gmail.com>
2014-10-23 03:32:02 +0300

    hb_1023

--- js/script.js
+++ js/script.js
@@ -5,4 +5,6 @@ $(function(){
 	$('.case-faq .item .capt a').click(function(){$(this).parents('.item').toggleClass('item_open');return false;});
 	$('.switcher label').click(function(){$(this).addClass('current').siblings().removeClass('current');});
 	if($('.chosen-select').length){$('.chosen-select').chosen({});};
+	if($('.popup-with-form').length){$('.popup-with-form').magnificPopup({type:'inline',preloader:false});$('#btn-send').click(function(){$('#popup-form').hide();$('#popup-thanks').show();return false;});};
+	$('.case-used .item').click(function(){$(this).toggleClass('current');});
 });
