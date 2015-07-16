29224bddfb43fbd4a2c92dae5206ae5b3a374b0e
Vadim <vadim.loginov.mail@gmail.com>
2015-07-17 01:31:26 +0300

    Wholesale done

--- js/wholesale.js
+++ js/wholesale.js
@@ -1,10 +1,25 @@
 window.jQuery(function ($) {
     'use strict';
-    
+
     // Открываем popup'ы
-    $('.open-feedback-done').magnificPopup({
-        type: 'inline',
-        midClick: true
+    $('.open-feedback-done').each(function () {
+        $(this).magnificPopup({
+            type: 'inline',
+            midClick: true
+        });
     });
-    
+
+    // Переключение вкладок в табличке с заказами
+    if ($('.table-zippy').length > 0) {
+        $('.table-zippy tr.heading').click(function () {
+            var closestTable = $(this).closest('table');
+            closestTable.siblings().removeClass('active');
+            if (closestTable.hasClass('active')) {
+                closestTable.removeClass('active');
+            } else {
+                closestTable.addClass('active');
+            }
+        });
+    }
+
 });
